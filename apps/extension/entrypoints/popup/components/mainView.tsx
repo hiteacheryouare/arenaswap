87abecb4457fa3cf9d3bfb14d3dfa7d2e5f7cc35
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-04-19 14:04:14 -0400

    switch image bases to /arenaswap base

--- apps/extension/entrypoints/popup/components/mainView.tsx
+++ apps/extension/entrypoints/popup/components/mainView.tsx
@@ -116,7 +116,7 @@ const mainView = ({
 	return (
 		<div className='popup-container'>
 			<div className='d-flex justify-content-between align-items-center mb-2 pb-2'>
-				<img src='/images/full_logo_white_on_transparent.png' alt='ArenaSwap' className='arenaswap-logo' />
+				<img src='/arenaswap/images/full_logo_white_on_transparent.png' alt='ArenaSwap' className='arenaswap-logo' />
 				<div className='d-flex align-items-center gap-2'>
 					<button className='btn btn-sm p-0 popup-settings-button' onClick={onOpenSetup} title='Settings'>
 						<i className='bi bi-gear-fill popup-settings-icon' />
