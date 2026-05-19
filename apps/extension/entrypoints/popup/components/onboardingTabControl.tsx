17a4218cdad263b5226383fde62b5c2cbef02033
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-05-18 22:08:52 -0400

    Update onboardingTabControl.tsx

--- apps/extension/entrypoints/popup/components/onboardingTabControl.tsx
+++ apps/extension/entrypoints/popup/components/onboardingTabControl.tsx
@@ -9,7 +9,7 @@ const onboardingTabControl = ({ onNext }: onboardingTabControlProps) => (
 			<img
 				src='/images/full_logo_white_on_transparent.png'
 				alt='ArenaSwap'
-				className='w-[9rem]'
+				className='w-36'
 			/>
 		</div>
 
