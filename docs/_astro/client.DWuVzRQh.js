0cd505e5acfacc2071b9f2ed849261c0b0f267ef
Ryan Mullin <82683251+hiteacheryouare@users.noreply.github.com>
2026-09-17 00:25:56 -0400

    build website

--- docs/_astro/client.DWuVzRQh.js
+++ docs/_astro/client.DWuVzRQh.js
@@ -1,5 +1,5 @@
-/*! ArenaSwap v2.1.0 Copyright (c) 2026 Ryan Mullin, Lattice & Company, and Contributors. All rights reserved. */
-import{t as e}from"./rolldown-runtime.nt6WcV1j.js";import{t}from"./react.CuAEMz5E.js";
+/*! ArenaSwap v2.1.1 Copyright (c) 2026 Ryan Mullin, Lattice & Company, and Contributors. All rights reserved. */
+import{t as e}from"./rolldown-runtime.6LFPHObu.js";import{t}from"./react.BuMBj47O.js";
 /**
 * @license React
 * scheduler.production.js
